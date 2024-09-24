6bc8099f396fd83769c596f599d7ffbba83ce4e4
thiszxw <3327332153@qq.com>
2024-09-25 01:23:40 +0800

    first commit

--- src/main/java/com/zxw/paoba/service/UserService.java
+++ src/main/java/com/zxw/paoba/service/UserService.java
@@ -1,5 +1,6 @@
 package com.zxw.paoba.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zxw.paoba.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -85,4 +86,5 @@ public interface UserService extends IService<User> {
      * @return
      */
     boolean isAdmin(User loginUser);
+
 }
--- src/main/java/com/zxw/paoba/service/impl/UserServiceImpl.java
+++ src/main/java/com/zxw/paoba/service/impl/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.zxw.paoba.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -13,11 +12,12 @@ import com.zxw.paoba.service.UserService;
 import com.zxw.paoba.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
@@ -36,6 +36,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     @Resource
     private UserMapper userMapper;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     /**
      * 盐值，混淆密码
@@ -267,11 +270,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         return loginUser != null && loginUser.getUserRole() == UserConstant.ADMIN_ROLE;
     }
 
-//    @Override
-//    public List<User> Page(Page<Object> objectPage, QueryWrapper<User> queryWrapper) {
-//        return null;
-//    }
-
 
     /**
      * 根据标签搜索用户（SQL 查询版）
@@ -294,11 +292,5 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         List<User> userList = userMapper.selectList(queryWrapper);
         //userList.forEach(user -> { getSafetyUser(user); });
         return userList.stream().map(this::getSafetyUser).collect(Collectors.toList());
-
     }
-
 }
-
-
-
-
